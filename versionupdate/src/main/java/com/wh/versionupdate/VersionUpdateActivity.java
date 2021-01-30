2e1032e5818fcd5e7af84c3ebdd98afde105fe9f
wang.hao <whstywh@163.com>
2021-01-30 14:30:05 +0800

    commit

---
+++ versionupdate/src/main/java/com/wh/versionupdate/VersionUpdateActivity.java
@@ -0,0 +1,14 @@
+package com.wh.versionupdate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VersionUpdateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vu_activity_versionupdate);
+    }
+}

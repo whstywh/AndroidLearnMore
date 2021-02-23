783f62e95c4e7723b88185f85456979ffab4efb6
whstywh <whstwh@163.com>
2021-02-23 18:25:37 +0800

    commit

---
+++ versionupdate/src/main/java/com/wh/versionupdate/DownLoadCompleteReceiver.java
@@ -0,0 +1,13 @@
+package com.wh.versionupdate;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class DownLoadCompleteReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
---
+++ versionupdate/src/main/java/com/wh/versionupdate/DownloadManagerClient.java
@@ -0,0 +1,45 @@
+package com.wh.versionupdate;
+
+import android.app.DownloadManager;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Environment;
+
+/**
+ * 2021/2/23
+ * wh
+ * desc：
+ */
+public class DownloadManagerClient {
+
+    private DownloadManagerClient mClient;
+    private long mId;
+
+    public DownloadManagerClient getInstance() {
+        if (mClient == null) {
+            synchronized (DownloadManagerClient.class) {
+                if (mClient == null) {
+                    mClient = new DownloadManagerClient();
+                }
+            }
+        }
+        return mClient;
+    }
+
+    public void download(Context context) {
+
+        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(""))
+                .setTitle("版本更新")
+                .setDescription("正在下载...")
+                .setAllowedOverRoaming(true)
+                .setDestinationInExternalFilesDir(context, Environment.DIRECTORY_NOTIFICATIONS, "version.apk");
+
+        DownloadManager downloadManager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
+        mId = downloadManager.enqueue(request);
+    }
+
+    public void cancelDownLodd(Context context) {
+        DownloadManager downloadManager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
+        downloadManager.remove(mId);
+    }
+}
